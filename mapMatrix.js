8366442a550c5fa9bab42ea4678dff2f76b50423
Arjit Jaiswal <arjitj2@gmail.com>
2016-03-13 14:39:16 -0500

    txt --> json

--- mapMatrix.js
+++ mapMatrix.js
@@ -1,10 +1,12 @@
 var sift = require('sift');
 
-var minutes = 2l
-var the_interval = minutes * 60 * 1000;
-console.log('sup');
+var minutes = 2
+var the_interval = minutes * 60 * 1000
+console.log('sup')
 
+var addresses = JSON.parse('addressesJSON.txt')
 
+console.log(addresses)
 
 setInterval(function() {
 
