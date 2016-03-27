c6d754f3ce1709d4a780c7425fd6a0c1369950d7
Arjit Jaiswal <arjitj2@gmail.com>
2016-03-27 14:05:53 -0500

    beginning actual matrix creation

--- mapMatrix.js
+++ mapMatrix.js
@@ -1,6 +1,16 @@
 var sift = require('sift');
+var GoogleMapsAPI = require('googlemaps')
 var addressData = require('./data.json'); //with path
-var arr = new Array(100);
+var matrix = new Array(100);
+
+var publicConfig = {
+  key: 'AIzaSyCpSSHy2RLV7562BeqzPEoVt5FfFDk3AtY',
+  stagger_time: 1000, // for elevationPath
+  encode_polylines: false,
+  secure: true, // use https
+};
+var gmAPI = new GoogleMapsAPI(publicConfig);
+var service = new gmAPI.maps.DirectionsService
 
 function begin_data_collection() {
   var seconds = 10*1000 //time is in milliseconds
@@ -10,10 +20,12 @@ function begin_data_collection() {
 }
 
 function create_data_matrix() {
-  for(var x = 0; x < arr.length; x++){
-      arr[x] = new Array(100);    
+  for(var x = 0; x < matrix.length; x++){
+      matrix[x] = new Array(100);    
   }
 }
+
+
 
 create_data_matrix()
 begin_data_collection()
