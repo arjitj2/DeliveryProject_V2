f98d05c3db0bdcf588bfa1cb9ae6703166643e48
Arjit Jaiswal <arjitj2@gmail.com>
2016-04-06 00:36:47 -0500

    script for matrix ready

--- mapMatrix.js
+++ mapMatrix.js
@@ -2,21 +2,48 @@ var sift = require('sift');
 var GoogleMapsAPI = require('googlemaps')
 var addressData = require('./data.json'); //with path
 var matrix = new Array(100);
+var x = 0
+var y = 0
 
 var publicConfig = {
-  key: 'AIzaSyCpSSHy2RLV7562BeqzPEoVt5FfFDk3AtY',
+  key: 'AIzaSyDqot4K4ln4OEi9nqnqghzv2X1Gucmvq84',
   stagger_time: 1000, // for elevationPath
   encode_polylines: false,
   secure: true, // use https
 };
+
 var gmAPI = new GoogleMapsAPI(publicConfig);
-var service = new gmAPI.maps.DirectionsService
 
 function begin_data_collection() {
-  var seconds = 10*1000 //time is in milliseconds
-  setInterval(function() {
-    console.log('hello there')
-  }, seconds);
+  var seconds = 5*1000 //time is in milliseconds
+  var interval = setInterval(
+    function() {
+      if(y < 100) {
+        gmAPI.directions({
+          origin: 'Madison , Wi, USA',
+          destination: 'Chicago, Il, USA'
+        }, callback);
+        y++
+      } else {
+        y = 0
+        x += 1
+        if(x < 100) {
+          console.log(x + " " + y) 
+        } else {
+          clearInterval(interval)
+        }
+      }
+    }, 
+  seconds);
+}
+
+function callback(err, result) {
+  if(result.status == 'OK') {
+    console.log(result.routes[0].legs[0].duration.text)
+    matrix[x][y] = result.routes[0].legs[0].duration.text
+  } else {
+    console.log('Directions request failed due to ' + result.status);
+  }
 }
 
 function create_data_matrix() {
